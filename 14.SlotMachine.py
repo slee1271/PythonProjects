132928fd3dff09966348f5428c8c014144ee6419
Shawn Lee <slee1271@mtroyal.ca>
2024-09-29 22:02:19 -0600

    done slot machine game

--- 14.SlotMachine.py
+++ 14.SlotMachine.py
@@ -1,11 +1,34 @@
+import random
 def spin_row():
-    pass 
+    symbols = ["🍒", "🍉", "🍋", "🔔", "⭐"]
 
-def print_row():
-    pass
+    # results = []
+    # for symbol in range(3):
+    #     results.append(random.choice(symbols))
+    # return results
 
-def get_payout():
-    pass
+    return [random.choice(symbols) for symbol in range(3)] # shortened the line above to one line of code 
+
+def print_row(row):
+    print("****************")
+    print("  | ".join(row))
+    print("****************")
+
+def get_payout(row, bet):
+    if row[0] == row[1] == row[2]:
+        if row[0] == "🍒":
+            return bet * 3
+        elif row[0] == "🍉":
+            return bet * 4
+        elif row[0] == "🍋":
+            return bet * 5
+        elif row[0] == "🔔":
+            return bet * 10
+        elif row[0] == "⭐":
+            return bet * 20
+    return 0
+        
+    
 
 def main():
     balance = 100
@@ -30,6 +53,27 @@ def main():
             continue
 
         balance -= bet
+
+        row = spin_row()
+        print("Spinning...\n")
+        print_row(row)
+
+        payout = get_payout(row, bet)
+
+        if payout > 0:
+            print(f"You won ${payout}")
+        else:
+            print("Sorry you lost this round")
+
+        balance += payout
+
+        play_again = input("Do you want to play again? (Y/N): ").upper()
+        if play_again != "Y":
+            break
+    
+    print(f"****************************************")
+    print(f"Game over! Your final balance is ${balance}")
+    print(f"****************************************")
 
 if __name__ == "__main__":
     main()
